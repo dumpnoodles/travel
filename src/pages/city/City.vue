184e1ec5af46824be6ce010b52a6f98a49ef8345
zijie <897872164@qq.com>
2020-04-23 16:25:23 +0800

    搜索组件完成

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="city">
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities='cities'></city-search>
     <city-list
     :hotCities='this.hotCities'
     :cities='this.cities'
